7e35014c74c81abd4208e84ce870925af7bc2d8f
Yakser <iaksanov2044@mail.ru>
2024-03-07 23:53:02 +0300

    feat: finished with section 3.3

--- reports/serializers.py
+++ reports/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class QualificationImprovementSerializer(serializers.Serializer):
     # Форма повышения квалификации
-    qualification_improvement_form = serializers.CharField()
+    form = serializers.CharField()
     country = serializers.CharField()
     organization = serializers.CharField()
     course_name = serializers.CharField()
@@ -13,6 +13,63 @@ class QualificationImprovementSerializer(serializers.Serializer):
     diploma_date = serializers.DateField()
 
     hours_count = serializers.IntegerField()
+
+
+class MethodicalWorkSerializer(serializers.Serializer):
+    # наименование
+    name = serializers.CharField()
+    # фио авторов
+    authors = serializers.CharField()  # фио авторов через запятую
+    # вид (учебник, пособие, методические указания и т.п.)
+    type = serializers.CharField()
+    # издательство, isbn
+    publisher = serializers.CharField()
+    # Объём в п.л. или стр.
+    pages_count = (
+        serializers.CharField()
+    )  # char field, потому что может быть "Электронное издание"
+
+
+class MonographSerializer(serializers.Serializer):
+    # наименование
+    name = serializers.CharField()
+    # фио авторов
+    authors_with_work = (
+        serializers.CharField()
+    )  # фио авторов через запятую с указанием места работы
+    # издательство, isbn
+    publisher = serializers.CharField()
+    # Объём в п.л. или стр.
+    pages_count = (
+        serializers.CharField()
+    )  # char field, потому что может быть "Электронное издание"
+
+
+class ArticleSerializer(serializers.Serializer):
+    # наименование
+    name = serializers.CharField()
+    # фио авторов
+    authors_with_work = (
+        serializers.CharField()
+    )  # фио авторов и соавторов через запятую с указанием места работы
+    # выходные данные, издательство, isbn
+    publisher = serializers.CharField()
+    # Объём в п.л. или стр.
+    pages_count = (
+        serializers.CharField()
+    )  # char field, потому что может быть "Электронное издание"
+
+
+class ConferenceSerializer(serializers.Serializer):
+    # наименование
+    name = serializers.CharField()
+    # дата проведения
+    date = serializers.DateField()
+    # место проведения
+    place = serializers.CharField()
+    status = serializers.CharField()
+    # вид участия
+    participation_type = serializers.CharField()
 
 
 class ReportSerializer(serializers.Serializer):
@@ -31,3 +88,19 @@ class ReportSerializer(serializers.Serializer):
     qualification_improvement = serializers.ListSerializer(
         child=QualificationImprovementSerializer()
     )
+    # 2 Учебно-методическая работа в 2021-22 уч.году
+    # 2.1 Перечень изданных учебно-методических пособий и указаний за 2021-22уч.год
+    methodical_works = serializers.ListSerializer(child=MethodicalWorkSerializer())
+
+    # 3.Сведения о научной и научно-методической работе
+    # 3.1. Сведения об опубликованных монографиях
+    monographs = serializers.ListSerializer(child=MonographSerializer())
+
+    # 3.2 Перечень статей в журналах, опубликованных в 2021-22 уч.году
+    scopus_articles = serializers.ListSerializer(child=ArticleSerializer())
+    web_of_science_articles = serializers.ListSerializer(child=ArticleSerializer())
+    vak_articles = serializers.ListSerializer(child=ArticleSerializer())
+    rinc_articles = serializers.ListSerializer(child=ArticleSerializer())
+
+    # 3.3 Перечень конференций, в которых принимал участие в 2021-22 уч. году. (в том числе с участием студентов)
+    conferences = serializers.ListSerializer(child=ConferenceSerializer())
