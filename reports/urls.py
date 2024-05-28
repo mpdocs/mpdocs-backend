56ff49b1b362052f95cf3bfd57a810e093adda1c
Sergey Yaksanov <iaksanov2044@mail.ru>
2024-05-29 00:37:36 +0300

    feat: return generated file

--- reports/urls.py
+++ reports/urls.py
@@ -7,5 +7,5 @@ app_name = "reports"
 urlpatterns = [
     path("", ReportsListView.as_view(), name="reports_list"),
     path("<int:pk>/", ReportDetailView.as_view(), name="report_detail"),
-    path("<int:pk>/generate", ReportGenerateView.as_view(), name="report_generate"),
+    path("<int:pk>/generate/", ReportGenerateView.as_view(), name="report_generate"),
 ]
