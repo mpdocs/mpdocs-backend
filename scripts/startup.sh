fcce6b11dd571ed075b3f6ecb0130c4e4f4b3b3a
Sergey Yaksanov <iaksanov2044@mail.ru>
2024-06-19 10:52:55 +0300

    feat: rollback ports

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,4 +2,4 @@
 
 poetry run python manage.py migrate
 python manage.py collectstatic --no-input
-poetry run gunicorn service.wsgi:application -w 4 --bind 0.0.0.0:8123
+poetry run gunicorn service.wsgi:application -w 4 --bind 0.0.0.0:8000
