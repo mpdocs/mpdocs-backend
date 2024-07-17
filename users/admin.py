def9ff2266c35566e5a20a2072d369048cfda0d4
Screxy <dvbvladis@mail.ru>
2024-07-17 21:31:46 +0300

    fix: admin form user password hash

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,29 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 User = get_user_model()
 
 
-# admin.site.unregister(User)
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'patronymic')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'patronymic', 'password')
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
     fieldsets = (
         (
             "Аккаунт",
@@ -47,6 +62,18 @@ class UserAdmin(admin.ModelAdmin):
         (
             "Важные даты",
             {"fields": ("last_login", "date_joined")},
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'patronymic', 'is_staff',
+                    'is_superuser', 'groups', 'user_permissions'),
+            },
         ),
     )
 
